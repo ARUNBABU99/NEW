f62750c558a7fe899943c16d0f7582cb58c35811
ARUNBABU99 <91900166+ARUNBABU99@users.noreply.github.com>
2022-02-25 16:21:46 +0530

    Add files via upload
    2 nd update with search button

---
+++ tables.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Table with database</title>
+<style>
+input[type=text] {
+  width: 20%;
+  padding: 5px 5px;
+  margin: 5px 0;
+  color: #588c7e;
+  box-sizing: border-box;
+}
+table {
+border-collapse: collapse;
+width: 100%;
+color: #588c7e;
+font-family: monospace;
+font-size: 25px;
+text-align: left;
+}
+th {
+background-color: #588c7e;
+color: white;
+}
+tr:nth-child(even) {background-color: #f2f2f2}
+</style>
+</head>
+<body>
+
+<form action="" method="post">
+            ID:
+            <input type=text name="t1" placeholder="enter your id">
+            <br>
+            <br>
+            <input type=submit name="s">
+            <?php
+            
+           
+                
+if(isset($_POST['s']))
+{
+    $a=$_POST['t1']; //accessing value from the text field
+}
+            ?>
+        </form>
+<table>
+<tr>
+<th>Id</th>
+<th>name</th>
+<th>Phone</th>
+</tr>
+<?php
+$conn = mysqli_connect("localhost", "root", "", "trail");
+// Check connection
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+}
+$sql = "SELECT id, name, phone FROM office WHERE id=$a";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+// output data of each row
+while($row = $result->fetch_assoc()) {
+echo "<tr><td>" . $row["id"]. "</td><td>" . $row["name"] . "</td><td>"
+. $row["phone"]. "</td></tr>";
+}
+echo "</table>";
+} else { echo "0 results"; }
+$conn->close();
+?>
+</table>
+</body>
+</html>
